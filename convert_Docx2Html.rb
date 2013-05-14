379f7b28ad3b2ea8b12991cdbd1d753942970140
Romain Sagean <romain.sagean@gmail.com>
2013-05-14 16:03:27 +0200

    change path of file

--- convert_Docx2Html.rb
+++ convert_Docx2Html.rb
@@ -1,11 +1,11 @@
 require 'nokogiri'
 
-doc   = Nokogiri::XML(File.read('document.xml'))
+doc   = Nokogiri::XML(File.read('word/document.xml'))
 
 xslt  = Nokogiri::XSLT(File.read('DocxHtml.xsl'))
 tmp =xslt.transform(doc)
 puts tmp
 
-f = File.open("html.xml", "w")
+f = File.open("page.html", "w")
 f.write(tmp)
 f.close
