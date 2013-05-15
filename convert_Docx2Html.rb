1133470f56844fc79cfdd8c7da50560c6e2c2be5
Romain Sagean <romain.sagean@gmail.com>
2013-05-15 15:12:36 +0200

    add argument to specify exctraction path

--- convert_Docx2Html.rb
+++ convert_Docx2Html.rb
@@ -18,14 +18,29 @@ class WordXmlManipulate
   end
   
   def unzip_file (file, destination)
-	@zip.extract('word/_rels/document.xml.rels', 'document.xml.rels')
+	#unzip rels file
+	@zip.extract('word/_rels/document.xml.rels', File.join(destination,'document.xml.rels')){true}
+	
+	#unzip media directorie
+	@zip.each { |f|
+     f_path=File.join(destination, f.name.sub(/word\//,''))
+	 if File.exist?(f_path) then
+       FileUtils.rm_rf f_path
+     end
+	 if f.name=~/\/media\/+[\w+\-.]{1,}/ then
+	   puts f.name
+	   puts f_path
+       FileUtils.mkdir_p(File.dirname(f_path))
+       @zip.extract(f, f_path)
+	 end
+   }
   end
   
   def extract
     xml = @zip.read('word/document.xml')
     #initialise Nokogiri reader with document.xml
-    doc   = Nokogiri::XML(xml)
-	
+	doc   = Nokogiri::XML(xml)
+
 	#open xsl file
 	xslt  = Nokogiri::XSLT(File.read('DocxHtml.xsl'))
 	
@@ -46,11 +61,13 @@ end
 
 if __FILE__ == $0
   file = ARGV[0]
-  save_file = ARGV[1] || file.sub(/\.docx/, '.html')
+  destination = ARGV[1] || File.dirname(__FILE__)
+  save_file = File.join(destination, file.sub(/\.docx/, '.html'))
   w = WordXmlManipulate.open(file)
-  w.unzip_file(file, '')
+  w.unzip_file(file, destination)
   html=w.extract
-  w.show html
+  #w.show html
+  puts save_file
   w.save(html,save_file)
   puts"complete"
 end
